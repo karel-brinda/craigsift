4dc3a8d8e1ef34eb271c0a59a891500a1a23671c
Karel Břinda <karel.brinda@gmail.com>
2018-04-21 13:18:54 -0400

    Remove duplicates

--- process_list.py
+++ process_list.py
@@ -19,129 +19,142 @@ re_desc=re.compile(r'hdrlnk">(.*?)</a')
 # - "available "
 
 out_phrases=[
-	'studio',
-	'BR available',
-	'room available',
-	'available room',
-	'BR in',
-	'BD in',
-	'room in',
-	'BRs in',
-	'BDs in',
-	'rooms in',
-	'roommate',
-	'shared',
-	'sharing',
-	'roommate',
-	'flatmate',
-	'share',
-	'Huron',
-	'luxury',
+    'studio',
+    'BR available',
+    'room available',
+    'available room',
+    'BR in',
+    'BD in',
+    'room in',
+    'BRs in',
+    'BDs in',
+    'rooms in',
+    'roommate',
+    'shared',
+    'sharing',
+    'roommate',
+    'flatmate',
+    'share',
+    'Huron',
+    'luxury',
 ]
 
 
 def remove_duplicates(items):
-	seen=set()
-	items2=[]
-	for item in items:
-		if not item['url'] in seen:
-			items2.append(item)
-		seen.add(item['url'])
-	return items2
+    seen=set()
+    items2=[]
+    for item in items:
+        if not item['url'] in seen:
+            items2.append(item)
+        seen.add(item['url'])
+    return items2
 
 
 def process_list(list_fn):
-	fn=os.path.basename(list_fn).replace(".html","")
-	with open(list_fn) as f:
-		cont=f.read()
-	cont_oneline=cont.replace("\n", " ")
-	ms=re_post.findall(cont_oneline)
-	#print(ms)
+    fn=os.path.basename(list_fn).replace(".html","")
+    with open(list_fn) as f:
+        cont=f.read()
+    cont_oneline=cont.replace("\n", " ")
+    ms=re_post.findall(cont_oneline)
+    #print(ms)
 
-	pr_items=[]
+    pr_items=[]
 
-	for it in ms:
-		#print(m)
-		d=process_item(it)
-		d["file"]=fn
-		pr_items.append(d)
+    for it in ms:
+        #print(m)
+        d=process_item(it)
+        d["file"]=fn
+        pr_items.append(d)
 
-	pr_items_cat=assign_categories(pr_items)
+    pr_items_cat=assign_categories(pr_items)
 
-	return pr_items_cat
+    return pr_items_cat
 
 
 def assign_categories(items):
-	"""
-	categories:
-	- ok - probably relevant
-	- spam - spam
-	- out - out of interest
-	- cat - wrong category of advertisement
-	"""
-	items_cat=[]
+    """
+    categories:
+    - ok - probably relevant
+    - spam - spam
+    - out - out of interest
+    - cat - wrong category of advertisement
+    """
+    items_cat=[]
 
-	for item in items:
-		cat='ok'
+    for item in items:
+        cat='ok'
 
-		desc_norm=item['desc'].replace("&amp;","")
-		for f in out_phrases:
-			if desc_norm.lower().find(f.lower())!=-1:
-				cat='out'
+        desc_norm=item['desc'].replace("&amp;","")
+        for f in out_phrases:
+            if desc_norm.lower().find(f.lower())!=-1:
+                cat='out'
 
-		if desc_norm==desc_norm.upper():
-			cat='spam'
+        if desc_norm==desc_norm.upper():
+            cat='spam'
 
-		if desc_norm.find("**")!=-1:
-			cat='spam'
+        if desc_norm.find("**")!=-1:
+            cat='spam'
 
-		#if item['url'].find("/gbs/abo/")==-1:
-		#	cat='cat'
+        #if item['url'].find("/gbs/abo/")==-1:
+        #   cat='cat'
 
-		item['cat']=cat
-		items_cat.append(item)
+        item['cat']=cat
+        items_cat.append(item)
 
-	return items_cat
+    return items_cat
 
 
 def process_item(it):
-	d={}
+    d={}
 
-	m=re_price.search(it)
-	try:
-		d["price"]=int(m.group(1))
-	except (IndexError, AttributeError):
-		d["price"]="NA"
+    m=re_price.search(it)
+    try:
+        d["price"]=int(m.group(1))
+    except (IndexError, AttributeError):
+        d["price"]="NA"
 
-	m=re_link.search(it)
-	try:
-		d["url"]=m.group(1)
-		#d["url"]="https://boston.craigslist.org"+m.group(1)
-	except (IndexError, AttributeError):
-		d["url"]="NA"
+    m=re_link.search(it)
+    try:
+        d["url"]=m.group(1)
+        #d["url"]="https://boston.craigslist.org"+m.group(1)
+    except (IndexError, AttributeError):
+        d["url"]="NA"
 
-	m=re_datetime.search(it)
-	try:
-		d["datetime"]=m.group(1)
-	except (IndexError, AttributeError):
-		d["datetime"]="NA"
+    m=re_datetime.search(it)
+    try:
+        d["datetime"]=m.group(1)
+    except (IndexError, AttributeError):
+        d["datetime"]="NA"
 
-	m=re_desc.search(it)
-	try:
-		d["desc"]=m.group(1)
-	except (IndexError, AttributeError):
-		d["desc"]="NA"
+    m=re_desc.search(it)
+    try:
+        d["desc"]=m.group(1)
+    except (IndexError, AttributeError):
+        d["desc"]="NA"
 
-	return d
+    return d
+
+
+def remove_duplicates(rs):
+    filtered=[]
+    seen=set()
+    for r in rs:
+        if not r["desc"] in seen:
+            filtered.append(r)
+            seen.add(r["desc"])
+    return filtered
 
 
 def print_as_html(rs):
-	print("""
-	<!DOCTYPE html>
+    print("""
+    <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Craigslist filtered results</title>
+<style>
+a:visited {{color:black;}}
+</style>
 </head>
 
 <body>
@@ -151,44 +164,46 @@ def print_as_html(rs):
 </body>
 
 </html>
-	""".format(
-			"\n".join(
-					['<tr><td>{}</td><td>{}</td><td>${}</td><td><a href="{}">{}</a></td><td>{}</td></tr>'.format(r['datetime'], r['cat'], r['price'], r['url'], r['desc'], r['file']) for r in rs]
-				)
-		)
-	)
+    """.format(
+            "\n".join(
+                    ['<tr><td>{}</td><td>{}</td><td>${}</td><td><a href="{}">{}</a></td><td>{}</td></tr>'.format(r['datetime'], r['cat'], r['price'], r['url'], r['desc'], r['file']) for r in rs]
+                )
+        )
+    )
 
 def main():
-	parser = argparse.ArgumentParser(description="")
+    parser = argparse.ArgumentParser(description="")
 
-	parser.add_argument('-c',
-			metavar='str',
-			dest='cat',
-			help='category (ok / spam / cat / out)',
-			default=None,
-		)
+    parser.add_argument('-c',
+            metavar='str',
+            dest='cat',
+            help='category (ok / spam / cat / out)',
+            default=None,
+        )
 
-	parser.add_argument('ls',
-			metavar='str',
-			help='',
-			nargs='+',
-		)
+    parser.add_argument('ls',
+            metavar='str',
+            help='',
+            nargs='+',
+        )
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	rs=itertools.chain(*[process_list(l) for l in args.ls])
-	rs=remove_duplicates(rs)
-	rs.sort(key=lambda x: x['datetime'], reverse=True)
+    rs=itertools.chain(*[process_list(l) for l in args.ls])
+    rs=remove_duplicates(rs)
+    rs.sort(key=lambda x: x['datetime'], reverse=True)
 
-	if args.cat is None:
-		print_as_html(rs)
-	else:
-		print_as_html(filter(lambda x: x['cat']==args.cat, rs))
+    rs=remove_duplicates(rs)
 
-	#for x in r:
-	#	if x['cat']=="ok":
-	#		print(x)
+    if args.cat is None:
+        print_as_html(rs)
+    else:
+        print_as_html(filter(lambda x: x['cat']==args.cat, rs))
+
+    #for x in r:
+    #   if x['cat']=="ok":
+    #       print(x)
 
 
 if __name__ == "__main__":
-	main()
+    main()
