32f612106543cbe4ba028438601ca2f7f546a1eb
Karel Břinda <karel.brinda@gmail.com>
2018-05-08 00:09:02 -0400

    Brookline

--- run.sh
+++ run.sh
@@ -2,8 +2,7 @@
 
 DIR=$(dirname $0)
 
-#zip_codes="02142 02141 02140 02139 02138 02134 02115 02199"
-zip_codes="02142 02141 02140 02139 02138"
+zip_codes="02446 02445"
 
 set -e
 set -o pipefail
@@ -14,7 +13,7 @@ mkdir lists
 	cd lists
 	for zip in $zip_codes; do
 		echo "Downloading $zip"
-		curl "https://boston.craigslist.org/search/nfa?search_distance=1&postal=$zip&min_price=1400&max_price=2100" > "$zip.html" &
+		curl "https://boston.craigslist.org/search/nfa?search_distance=0&postal=$zip&min_price=1400&max_price=2100&fake-param=$(date +%s)" > "$zip.html" &
 	done;
 	wait
 )
